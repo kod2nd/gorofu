6d388cd9d5ab923fbcd325b49909760ed7b810bc
kod2nd <kod2nd@gmail.com>
2025-11-04 21:54:55 +0800

    use display flex

--- src/components/ProfileEditForm.jsx
+++ src/components/ProfileEditForm.jsx
@@ -3,7 +3,6 @@ import {
   Box,
   Paper,
   Typography,
-  Grid,
   TextField,
   Autocomplete,
 } from "@mui/material";
@@ -15,133 +14,143 @@ const ProfileEditForm = ({ formData, email, handleInputChange }) => {
       <Typography variant="h6" fontWeight="bold" gutterBottom sx={{ mb: 3 }}>
         Edit Personal Information
       </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6} sx={{ width: '45%'}}>
-          <TextField
-            label="Display Name"
-            name="full_name"
-            value={formData.full_name}
-            onChange={handleInputChange}
-            fullWidth
-            variant="outlined"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} sx={{ width: '45%'}}>
-          <TextField
-            label="Email"
-            value={email}
-            fullWidth
-            disabled
-            variant="outlined"
-            helperText="Email cannot be changed"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} sx={{ width: '45%'}}>
-          <TextField
-            label="Handicap"
-            name="handicap"
-            type="number"
-            value={formData.handicap}
-            onChange={handleInputChange}
-            fullWidth
-            variant="outlined"
-            inputProps={{ step: 0.1 }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} sx={{ width: '45%'}}>
-          <Autocomplete
-            options={countries}
-            getOptionLabel={(option) => option.label}
-            value={countries.find((c) => c.label === formData.country) || null}
-            onChange={(event, newValue) => {
-              handleInputChange({
-                target: {
-                  name: "country",
-                  value: newValue ? newValue.label : "",
-                },
-              });
-            }}
-            renderOption={(props, option) => {
-              return (
-                <Box
-                  component="li"
-                  {...props}
-                  sx={{
-                    "& > img": { mr: 2, flexShrink: 0 },
-                    py: 1.5,
-                    minHeight: 47,
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <img
-                    loading="lazy"
-                    width="20"
-                    src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
-                    srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
-                    alt=""
-                  />
-                  <Typography noWrap sx={{ flex: 1, minWidth: 0 }}>
-                    {option.label}
-                  </Typography>
-                </Box>
-              );
-            }}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Country"
-                variant="outlined"
+      
+      {/* Use flexbox with gap for consistent spacing */}
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: 3,
+          '& > *': {
+            flexBasis: { xs: '100%', sm: 'calc(50% - 12px)' }, // 50% minus half the gap
+          }
+        }}
+      >
+        {/* Display Name */}
+        <TextField
+          label="Display Name"
+          name="full_name"
+          value={formData.full_name}
+          onChange={handleInputChange}
+          fullWidth
+          variant="outlined"
+        />
+        
+        {/* Email */}
+        <TextField
+          label="Email"
+          value={email}
+          fullWidth
+          disabled
+          variant="outlined"
+          helperText="Email cannot be changed"
+        />
+        
+        {/* Handicap */}
+        <TextField
+          label="Handicap"
+          name="handicap"
+          type="number"
+          value={formData.handicap}
+          onChange={handleInputChange}
+          fullWidth
+          variant="outlined"
+          inputProps={{ step: 0.1 }}
+        />
+        
+        {/* Country */}
+        <Autocomplete
+          options={countries}
+          getOptionLabel={(option) => option.label}
+          value={countries.find((c) => c.label === formData.country) || null}
+          onChange={(event, newValue) => {
+            handleInputChange({
+              target: {
+                name: "country",
+                value: newValue ? newValue.label : "",
+              },
+            });
+          }}
+          renderOption={(props, option) => {
+            return (
+              <Box
+                component="li"
+                {...props}
                 sx={{
-                  "& .MuiAutocomplete-inputRoot": {
-                    paddingRight: "32px !important",
-                  },
-                }}
-              />
-            )}
-            ListboxProps={{
-              sx: {
-                maxHeight: 300,
-                "& li": {
-                  whiteSpace: "normal",
-                  wordWrap: "break-word",
-                  minHeight: 48,
+                  "& > img": { mr: 2, flexShrink: 0 },
+                  py: 1.5,
+                  minHeight: 47,
                   display: "flex",
                   alignItems: "center",
+                }}
+              >
+                <img
+                  loading="lazy"
+                  width="20"
+                  src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
+                  srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
+                  alt=""
+                />
+                <Typography noWrap sx={{ flex: 1, minWidth: 0 }}>
+                  {option.label}
+                </Typography>
+              </Box>
+            );
+          }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Country"
+              variant="outlined"
+              sx={{
+                "& .MuiAutocomplete-inputRoot": {
+                  paddingRight: "32px !important",
                 },
+              }}
+            />
+          )}
+          ListboxProps={{
+            sx: {
+              maxHeight: 300,
+              "& li": {
+                whiteSpace: "normal",
+                wordWrap: "break-word",
+                minHeight: 48,
+                display: "flex",
+                alignItems: "center",
               },
-            }}
-            sx={{
-              "& .MuiAutocomplete-input": {
-                minWidth: "120px !important",
-              },
-            }}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} sx={{ width: '45%'}}>
-          <TextField
-            label="Phone Number"
-            name="phone"
-            value={formData.phone}
-            onChange={handleInputChange}
-            fullWidth
-            variant="outlined"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} sx={{ width: '45%'}}>
-          <TextField
-            label="Date of Birth"
-            name="date_of_birth"
-            type="date"
-            value={formData.date_of_birth}
-            onChange={handleInputChange}
-            fullWidth
-            variant="outlined"
-            InputLabelProps={{ shrink: true }}
-          />
-        </Grid>
-      </Grid>
+            },
+          }}
+          sx={{
+            "& .MuiAutocomplete-input": {
+              minWidth: "120px !important",
+            },
+          }}
+          fullWidth
+        />
+        
+        {/* Phone */}
+        <TextField
+          label="Phone Number"
+          name="phone"
+          value={formData.phone}
+          onChange={handleInputChange}
+          fullWidth
+          variant="outlined"
+        />
+        
+        {/* Date of Birth */}
+        <TextField
+          label="Date of Birth"
+          name="date_of_birth"
+          type="date"
+          value={formData.date_of_birth}
+          onChange={handleInputChange}
+          fullWidth
+          variant="outlined"
+          InputLabelProps={{ shrink: true }}
+        />
+      </Box>
     </Paper>
   );
 };
